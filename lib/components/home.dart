b93e126b4373415358edd5f4841e1d039901923f
bendeez <blaziken@MSI>
2025-02-22 16:31:46 -0600

    almost finished the home page

--- lib/components/home.dart
+++ lib/components/home.dart
@@ -14,56 +14,50 @@ class HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-      Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        Text("Hi, I'm Daniel Lacina",
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40)),
-        AnimatedTextKit(animatedTexts: [
-          ColorizeAnimatedText("Software Developer",
-              textStyle: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-              colors: [Colors.blue, Colors.white])
-        ]),
-        Text("I create bots and websites.")
-      ]),
-      Stack(
-        alignment: Alignment.center,
-        children: [
-          ColorFiltered(
-            colorFilter:
-                ColorFilter.mode(Colors.blue.withOpacity(.5), BlendMode.darken),
-            child: Image.asset(
-              'assets/images/profile_picture.jpg',
-              width: 300,
-              height: 300,
-              fit: BoxFit.cover,
-            ),
-          ),
-          Container(
-            width: 300,
-            height: 300,
-            decoration: BoxDecoration(
-              gradient: RadialGradient(
-                center: Alignment.center,
-                radius: 1,
-                colors: [
-                  Colors.yellow.withOpacity(.4),
-                  Colors.blue.withOpacity(.1),
-                  Colors.black,
-                ],
-                stops: [0.0, .2, .7],
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            spacing: 30,
+            children: [
+          Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Text("Hi, I'm Daniel Lacina",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40)),
+            AnimatedTextKit(animatedTexts: [
+              ColorizeAnimatedText("Software Developer",
+                  textStyle:
+                      TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                  colors: [Colors.blue, Colors.white])
+            ]),
+            Container(
+                margin: EdgeInsets.only(top: 10),
+                child: Text(
+                  "I create bots and websites.",
+                ))
+          ]),
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              Image.asset(
+                'assets/images/profile_picture.jpg',
+                width: 300,
+                height: 300,
+                fit: BoxFit.cover,
               ),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.yellow.withOpacity(.4),
-                  offset: Offset(0, 0),
-                  blurRadius: 3,
-                  spreadRadius: 0,
-                ),
-              ],
-            ),
-          ),
-        ],
-      )
-    ]));
+              Container(
+                  width: 300,
+                  height: 300,
+                  decoration: BoxDecoration(
+                    gradient: RadialGradient(
+                      center: Alignment.center,
+                      radius: 1,
+                      colors: [
+                        Colors.blue.withOpacity(.1),
+                        Colors.black,
+                      ],
+                      stops: [0.0, .8],
+                    ),
+                  ))
+            ],
+          )
+        ]));
   }
 }
