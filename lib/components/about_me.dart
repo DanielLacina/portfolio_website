2645f5ab8bd8fd751ad348c7db56e277f9b5c8e0
bendeez <blaziken@MSI>
2025-02-25 06:16:34 -0600

    added about me page

--- lib/components/about_me.dart
+++ lib/components/about_me.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:glowy_borders/glowy_borders.dart';
 
 class AboutMePage extends StatefulWidget {
   const AboutMePage({super.key});
@@ -11,14 +12,60 @@ class AboutMePage extends StatefulWidget {
 class AboutMePageState extends State<AboutMePage> {
   @override
   Widget build(BuildContext context) {
+    return Column(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+      const Title(),
+      const SizedBox(height: 10),
+      const ProfilePicture(),
+      const SizedBox(height: 10),
+      Text("Software Developer",
+          style: TextStyle(
+              fontSize: 36, fontWeight: FontWeight.bold, color: Colors.white)),
+      const SizedBox(height: 10),
+      Text("I'm passionate about ml/ai and software engineering.",
+          style: TextStyle(fontSize: 16)),
+    ]);
+  }
+}
+
+class Title extends StatelessWidget {
+  const Title({super.key});
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
         child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
       Text("About ",
           style: TextStyle(
-              color: Colors.white, fontSize: 36, fontWeight: FontWeight.bold)),
+              color: Colors.white, fontSize: 48, fontWeight: FontWeight.bold)),
       Text("Me",
           style: TextStyle(
-              color: Colors.blue, fontSize: 36, fontWeight: FontWeight.bold))
+              color: Colors.blue, fontSize: 48, fontWeight: FontWeight.bold))
     ]));
+  }
+}
+
+class ProfilePicture extends StatelessWidget {
+  const ProfilePicture({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedGradientBorder(
+        borderSize: 1,
+        glowSize: 1,
+        borderRadius: BorderRadius.all(Radius.circular(999)),
+        gradientColors: [Colors.blue, Colors.transparent],
+        child: AnimatedGradientBorder(
+            borderSize: 1,
+            glowSize: 1,
+            borderRadius: BorderRadius.all(Radius.circular(999)),
+            gradientColors: [Colors.blue],
+            child: Container(
+                child: ClipOval(
+                    child: Image.asset(
+              'assets/images/profile_picture.jpg',
+              width: 200,
+              height: 200,
+              fit: BoxFit.cover,
+            )))));
   }
 }
